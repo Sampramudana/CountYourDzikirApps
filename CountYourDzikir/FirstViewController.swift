2a408583b49ffa96fb2249a7e4681f1be6014c6a
Sampramudana <32828518+Sampramudana@users.noreply.github.com>
2017-11-24 16:36:09 +0700

    CountYourDzikirApps

---
+++ CountYourDzikir/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CountYourDzikir
+//
+//  Created by Becak Holic on 11/24/17.
+//  Copyright © 2017 Sam Pramudana. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
